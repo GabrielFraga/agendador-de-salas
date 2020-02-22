efa2d262b30ef5c87ffb2d123add084bb284666f
GabrielFraga <gabrielgoog@gmail.com>
2020-02-22 10:47:24 -0300

    agendamentos:alinhamento de itens na tela

--- frontend/src/components/AppointmentForm/index.js
+++ frontend/src/components/AppointmentForm/index.js
@@ -31,13 +31,14 @@ export default function AppointmentForm({ action }) {
       </Header>
       <Content>
         <Form className="p-4">
-          <Row>
+          <Row className="justify-content-between p-2">
             <div className="form-group">
               <label className="form-lavel"> Cadeiras</label>
               <Input
                 className="form-control"
                 id="qtd_chairs"
                 name="qtd_chairs"
+                placeholder="Quantidade de cadeiras"
               />
             </div>
             <div className="form-group">
@@ -49,62 +50,56 @@ export default function AppointmentForm({ action }) {
               />
             </div>
             <div className="form-group">
-              <label htmlFor="" className="form-label">
-                Data Final
-              </label>
+              <label className="form-label">Data Final</label>
               <Input className="form-control" id="date_end" name="date_end" />
             </div>
           </Row>
-          <div className="form-group">
-            <div className="form-check">
-              <Input
-                className="form-control"
-                id="collaborator_id"
-                name="collaborator_id"
-              />
+          <Row className="justify-content-between p-2">
+            <div className="form-group">
+              <div className="form-check">
+                <Input
+                  className="form-check-input"
+                  type="checkbox"
+                  id="collaborator_id"
+                  name="collaborator_id"
+                />
+                <label className="form-label">Colaborador</label>
+              </div>
             </div>
-            <label htmlFor="" className="form-label">
-              Colaborador
-            </label>
-          </div>
-          <div className="form-group">
-            <div className="form-check">
-              <Input
-                className="form-control"
-                type="checkbox"
-                id="has_computer"
-                name="has_computer"
-              />
+            <div className="form-group">
+              <div className="form-check">
+                <Input
+                  className="form-check-input"
+                  type="checkbox"
+                  id="has_computer"
+                  name="has_computer"
+                />
+                <label className="form-label">Possui computador</label>
+              </div>
             </div>
-            <label htmlFor="" className="form-label">
-              Possui computador
-            </label>
-          </div>
-          <div className="form-group">
-            <div className="form-check">
-              <Input
-                className="form-control"
-                type="checkbox"
-                id="has_projector"
-                name="has_projector"
-              />
+            <div className="form-group">
+              <div className="form-check">
+                <Input
+                  className="form-check-input"
+                  type="checkbox"
+                  id="has_projector"
+                  name="has_projector"
+                />
+                <label className="form-label">Possui projetor</label>
+              </div>
             </div>
-            <label htmlFor="" className="form-label">
-              Possui projetor
-            </label>
-          </div>
-          <div className="form-group">
-            <Input
-              className="form-control"
-              type="checkbox"
-              id="has_video_chat"
-              name="has_video_chat"
-            />
-          </div>
-          <label htmlFor="" className="form-label">
-            Possui vide chamada
-          </label>
-          <div className="form-check" />
+            <div className="form-group">
+              <div className="form-check">
+                <Input
+                  className="form-check-input"
+                  type="checkbox"
+                  id="has_video_chat"
+                  name="has_video_chat"
+                />
+                <label className="form-label">Possui video chamada</label>
+              </div>
+            </div>
+          </Row>
         </Form>
       </Content>
     </Container>
