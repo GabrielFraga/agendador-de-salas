4ff7227d44db8432436d8fa4b0b91bdc986159a2
GabrielFraga <gabrielgoog@gmail.com>
2020-02-23 20:19:15 -0300

    agendamentos:detalhes da tela

--- frontend/src/components/AppointmentForm/index.js
+++ frontend/src/components/AppointmentForm/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import * as Yup from 'yup';
 
-import { Button, Row } from 'react-bootstrap';
+import { Button, Row, Col } from 'react-bootstrap';
 
 import { Form, Input } from '@rocketseat/unform';
 
@@ -32,73 +32,93 @@ export default function AppointmentForm({ action }) {
       <Content>
         <Form className="p-4">
           <Row className="justify-content-between p-2">
-            <div className="form-group">
-              <label className="form-lavel"> Cadeiras</label>
-              <Input
-                className="form-control"
-                id="qtd_chairs"
-                name="qtd_chairs"
-                placeholder="Quantidade de cadeiras"
-              />
-            </div>
-            <div className="form-group">
-              <label className="form-label">Data de Início</label>
-              <Input
+            <Col>
+              <div className="form-group">
+                <label className="form-lavel"> Cadeiras</label>
+                <Input
+                  className="form-control"
+                  id="qtd_chairs"
+                  name="qtd_chairs"
+                  placeholder="Quantidade de cadeiras"
+                />
+              </div>
+            </Col>
+            <Col>
+              <div className="form-group">
+                <label className="form-label">Data de Início</label>
+                {/* <Input
                 className="form-control"
                 id="date_start"
                 name="date_start"
-              />
-            </div>
-            <div className="form-group">
-              <label className="form-label">Data Final</label>
-              <Input className="form-control" id="date_end" name="date_end" />
-            </div>
+              /> */}
+                <div className="input-group date" id="datetimepicker2">
+                  <input type="text" className="form-control" />
+                  <span className="input-group-addon">
+                    <span className="glyphicon glyphicon-calendar" />
+                  </span>
+                </div>
+              </div>
+            </Col>
+            <Col>
+              <div className="form-group">
+                <label className="form-label">Data Final</label>
+                <Input className="form-control" id="date_end" name="date_end" />
+              </div>
+            </Col>
           </Row>
-          <Row className="justify-content-between p-2">
-            <div className="form-group">
-              <div className="form-check">
-                <Input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="collaborator_id"
-                  name="collaborator_id"
-                />
-                <label className="form-label">Colaborador</label>
+          <Row className="p-2">
+            <Col>
+              <div className="form-group">
+                <div className="form-check">
+                  <Input
+                    className="form-check-input"
+                    type="checkbox"
+                    id="collaborator_id"
+                    name="collaborator_id"
+                  />
+                  <label className="form-label">Colaborador</label>
+                </div>
               </div>
-            </div>
-            <div className="form-group">
-              <div className="form-check">
-                <Input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="has_computer"
-                  name="has_computer"
-                />
-                <label className="form-label">Possui computador</label>
+            </Col>
+            <Col>
+              <div className="form-group">
+                <div className="form-check">
+                  <Input
+                    className="form-check-input"
+                    type="checkbox"
+                    id="has_computer"
+                    name="has_computer"
+                  />
+                  <label className="form-label">Possui computador</label>
+                </div>
               </div>
-            </div>
-            <div className="form-group">
-              <div className="form-check">
-                <Input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="has_projector"
-                  name="has_projector"
-                />
-                <label className="form-label">Possui projetor</label>
+            </Col>
+            <Col>
+              <div className="form-group">
+                <div className="form-check">
+                  <Input
+                    className="form-check-input"
+                    type="checkbox"
+                    id="has_projector"
+                    name="has_projector"
+                  />
+                  <label className="form-label">Possui projetor</label>
+                </div>
               </div>
-            </div>
-            <div className="form-group">
-              <div className="form-check">
-                <Input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="has_video_chat"
-                  name="has_video_chat"
-                />
-                <label className="form-label">Possui video chamada</label>
+            </Col>
+            <Col>
+              <div className="form-group">
+                <div className="form-check">
+                  <Input
+                    className="form-check-input"
+                    type="checkbox"
+                    id="has_video_chat"
+                    name="has_video_chat"
+                  />
+                  <label className="form-label">Possui video chamada</label>
+                </div>
               </div>
-            </div>
+            </Col>
           </Row>
         </Form>
       </Content>
